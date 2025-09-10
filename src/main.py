f1db8bedf84d0209be7998d69bc261a2fc085c38
Joaquim Ribeiro <joaquimgamer23@gmail.com>
2025-09-10 11:23:27 -0300

    added request function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
 from src.config import USED_MODELS, DOME_CONFIGURATIONS
 from src.db.db_utils.create_dynamic_db import create_bronze_table
+from src.dao.bronze_dao import BronzeDAO
+import requests
+import pandas as pd
+
+def message_dome(message: str, configuration: str):
+    request_url = "http://127.0.0.1:5000/new_lim/message"
+    data = {
+        "message": message,
+        "context": {
+            "chat_id": 1
+        }
+    }
+    answer = requests.post(request_url, json=data).json()
+    return answer
+
+def process_dataset(dataset_path: str, configuration: str, ):
+    df = pd.read_csv(dataset_path)
+    messasges = df['user_msg'].tolist()
+    # import dao and get all
+    responses = []
+    for message in messasges:
+        if(message):
+            response = message_dome(message, configuration)
+            responses.append(response)
+            # save to database
+    return responses
 
 def iterate_model_configurations():
     for model in USED_MODELS:
