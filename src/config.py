6bb9035df78338d4135021f44e1c2273a0090569
Gebelubo <gabrielluiz2004@gmail.com>
2025-09-15 11:14:45 -0300

    models updated

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-USED_MODELS = ['llama3', 'gemma3', 'qwen3']
+USED_MODELS = ['llama3', 'gemma3', 'qwen3', 'mistral', 'flan', 'phi3']
 DOME_CONFIGURATIONS = ['default', 'treatment_mode']
 
 DATABASE_URL = "postgresql+psycopg2://user:password@postgres_db:5432/data_tables"
